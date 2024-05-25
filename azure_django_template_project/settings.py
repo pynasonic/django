29a500ee12d2284ec954d235a8050876170c197b
MERNTS <ucrossgram@gmail.com>
2024-05-25 11:40:07 -0400

    PostgreSQL

--- azure_django_template_project/settings.py
+++ azure_django_template_project/settings.py
@@ -61,16 +61,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'azure_django_template_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'HOST': 'momento-momento.i.aivencloud.com',
+        'PORT': '23450',
+        'USER': 'avnadmin',
+        'PASSWORD': os.environ['PG_PWD_MM'],
+
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
