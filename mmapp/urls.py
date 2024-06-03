ff952663b05fae1da9e154cdaf0d9e595491d263
aiXpertLab <aiXpertLab@gmail.com>
2024-06-03 00:52:10 -0400

    list

--- mmapp/urls.py
+++ mmapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shelters', views.shelter_list, name = 'shelter_list'),
+    path('shelter/<int:pk>', views.shelter_detail, name = 'shelter_detail'),
 ]
--- mmapp/views.py
+++ mmapp/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from . import models
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def shelter_list(request):
+    shelters = models.Shelter.objects.all()
+    context = {'shelters': shelters}
+    return render(request, 'shelter_list.html', context)
+
+def shelter_detail(request, pk):
+    shelter = get_object_or_404(models.Shelter, pk=pk)
+    context = {'shelter': shelter}
+    return render(request, 'shelter_detail.html', context)
+
